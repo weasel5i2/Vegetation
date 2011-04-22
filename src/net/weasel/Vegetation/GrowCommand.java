245031bf9206f897473f9a0f197b8518120952a1
zeff89 <zeff89@gmx.de>
2011-04-23 01:14:16 +0200

    Damn auto formating...

--- src/net/weasel/Vegetation/GrowCommand.java
+++ src/net/weasel/Vegetation/GrowCommand.java
@@ -22,76 +22,63 @@ public class GrowCommand implements CommandExecutor {
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String commandLabel, String[] args)
 	{
-		if(sender instanceof Player)
+		if( sender instanceof Player )
 		{
-			Player P = (Player) sender;
+			Player P = (Player)sender;
 
-			if (Vegetation.Permissions.has(P, "vegetation.grow"))
+			if ( Vegetation.Permissions.has(P, "vegetation.grow") )
 			{
 				// Todo: implement command queue
-				if (Vegetation.ActivePlayerCommands < 20) {
+				if ( Vegetation.ActivePlayerCommands < 20 )
+				{
 					Vegetation.ActivePlayerCommands++;
-					sender.sendMessage("Growing everything..");
-					for (int I = 0; I < 100; I++) {
-						Block CB = Blocks.getRandomBlock(((Player) sender)
-								.getLocation());
+					sender.sendMessage( "Growing everything.." );
+					for (int I = 0; I < 100; I++)
+					{
+						Block CB = Blocks.getRandomBlock(((Player) sender).getLocation());
 
-						if (CB != null) {
-							switch (CB.getType()) {
+						if ( CB != null )
+						{
+							switch ( CB.getType() )
+							{
 							case GRASS:
-								if (Vegetation.debugging)
-									logOutput("Found Block of Type: "
-											+ Material.GRASS);
-								Grass.growGrass(CB);
+								if (Vegetation.debugging) logOutput( "Found Block of Type: " + Material.GRASS );
+								Grass.growGrass( CB );
 								break;
 
 							case CACTUS:
-								if (Vegetation.debugging)
-									logOutput("Found Block of Type: "
-											+ Material.CACTUS);
-								Cacti.growCacti(CB);
+								if( Vegetation.debugging ) logOutput( "Found Block of Type: " + Material.CACTUS );
+								Cacti.growCacti( CB );
 								break;
 
 							case SUGAR_CANE_BLOCK:
-								if (Vegetation.debugging)
-									logOutput("Found Block of Type: "
-											+ Material.SUGAR_CANE_BLOCK);
-								Cranes.GrowCranes(CB);
+								if( Vegetation.debugging ) logOutput( "Found Block of Type: " + Material.SUGAR_CANE_BLOCK );
+								Cranes.GrowCranes( CB );
 								break;
 
 							case YELLOW_FLOWER:
-								if (Vegetation.debugging)
-									logOutput("Found Block of Type: "
-											+ Material.YELLOW_FLOWER);
-								Plants.growPlant(CB, Material.YELLOW_FLOWER);
+								if( Vegetation.debugging ) logOutput( "Found Block of Type: " + Material.YELLOW_FLOWER );
+								Plants.growPlant( CB, Material.YELLOW_FLOWER );
 								break;
 
 							case RED_ROSE:
-								if (Vegetation.debugging)
-									logOutput("Found Block of Type: "
-											+ Material.RED_ROSE);
-								Plants.growPlant(CB, Material.RED_ROSE);
+								if( Vegetation.debugging ) logOutput( "Found Block of Type: " + Material.RED_ROSE );
+								Plants.growPlant( CB, Material.RED_ROSE );
 								break;
 
 							case BROWN_MUSHROOM:
-								if (Vegetation.debugging)
-									logOutput("Found Block of Type: "
-											+ Material.BROWN_MUSHROOM);
-								Plants.growPlant(CB, Material.BROWN_MUSHROOM);
+								if ( Vegetation.debugging ) logOutput( "Found Block of Type: " + Material.BROWN_MUSHROOM );
+								Plants.growPlant( CB, Material.BROWN_MUSHROOM );
 								break;
 
 							case RED_MUSHROOM:
-								if (Vegetation.debugging)
-									logOutput("Found Block of Type: "
-											+ Material.RED_MUSHROOM);
-								Plants.growPlant(CB, Material.RED_MUSHROOM);
+								if ( Vegetation.debugging ) logOutput( "Found Block of Type: " + Material.RED_MUSHROOM );
+								Plants.growPlant( CB, Material.RED_MUSHROOM );
 								break;
 
 							case PUMPKIN:
-								if (Vegetation.debugging)
-									logOutput("Found Block of Type: "
-											+ Material.PUMPKIN);
-								Plants.growPlant(CB, Material.PUMPKIN);
+								if (Vegetation.debugging) logOutput( "Found Block of Type: " + Material.PUMPKIN );
+								Plants.growPlant( CB, Material.PUMPKIN );
 								break;
 
 							default:
