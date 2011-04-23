01dff2d0f0944e24c58633e80b92c319acedbee7
zeff89 <zeff89@gmx.de>
2011-04-23 16:14:29 +0200

    implemented "grow" command

--- src/net/weasel/Vegetation/GrowCommand.java
+++ src/net/weasel/Vegetation/GrowCommand.java
@@ -1,5 +1,8 @@
 package net.weasel.Vegetation;
 
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -9,6 +12,8 @@ import org.bukkit.entity.Player;
 public class GrowCommand implements CommandExecutor{
 
 	public static void logOutput( String text ) { Vegetation.logOutput( text ); }
+	
+	public static final int MaxCycle = 100;
 	
 	
 	public GrowCommand( Vegetation Plugin )
@@ -25,7 +30,77 @@ public class GrowCommand implements CommandExecutor{
 
 			if ( Vegetation.Permissions.has(P, "vegetation.grow") )
 			{
-
+				if( args.length == 1)
+				{
+					String Arg = args[0];
+					Location PL = ((Player) sender).getLocation();
+					
+					if( Arg.equals("grass") )
+					{
+						sender.sendMessage( "Growing grass.." );
+						for (int I = 0; I < MaxCycle; I++)
+						{
+							Block CB = Blocks.getRandomBlock( PL );
+							if(  CB != null && CB.getType() == Material.GRASS )
+								Grass.growGrass( CB );
+						}
+						return true;
+					}
+					else if( Arg.equals("flower") )
+					{
+						sender.sendMessage( "Growing flowers.." );
+						for (int I = 0; I < MaxCycle; I++)
+						{
+							Block CB = Blocks.getRandomBlock( PL );
+							if(  CB != null && CB.getType() == Material.GRASS )
+							{
+								if( I%2 == 0 )
+									Plants.growPlant( CB, Material.YELLOW_FLOWER );
+								else
+									Plants.growPlant( CB, Material.RED_ROSE );
+							}
+						}
+						return true;
+					}
+					else if( Arg.equals("mushroom") )
+					{
+						sender.sendMessage( "Growing mushrooms.." );
+						for (int I = 0; I < MaxCycle; I++)
+						{
+							Block CB = Blocks.getRandomBlock( PL );
+							if(  CB != null && CB.getType() == Material.GRASS )
+							{
+								if( I%2 == 0 )
+									Plants.growPlant( CB, Material.RED_MUSHROOM);
+								else
+									Plants.growPlant( CB, Material.BROWN_MUSHROOM );
+							}
+						}
+						return true;
+					}
+					else if( Arg.equals("cactus") )
+					{
+						sender.sendMessage( "Growing cacti.." );
+						for (int I = 0; I < MaxCycle; I++)
+						{
+							Block CB = Blocks.getRandomBlock( PL );
+							if(  CB != null && CB.getType() == Material.GRASS )
+								Plants.growPlant( CB, Material.CACTUS );
+						}
+						return true;
+					}
+					else if( Arg.equals("sugar_cane") )
+					{
+						sender.sendMessage( "Growing sugar canes.." );
+						for (int I = 0; I < MaxCycle; I++)
+						{
+							Block CB = Blocks.getRandomBlock( PL );
+							if(  CB != null && CB.getType() == Material.GRASS )
+								Canes.GrowCanes( CB );
+						}
+						return true;
+					}
+				}
 			}
 		}
 		return false;
