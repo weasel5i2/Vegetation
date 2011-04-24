d487602ecf30630c4e5749dea4458446f4674847
zeff89 <zeff89@gmx.de>
2011-04-24 23:33:02 +0200

    adjusted spread amount

--- src/net/weasel/Vegetation/Moss.java
+++ src/net/weasel/Vegetation/Moss.java
@@ -10,7 +10,7 @@ public final class Moss
     public static void growMoss(Block B)
     {
     	int MaxCycle = 150;
-    	int MaxSpreadAmount = 3;
+    	int MaxSpreadAmount = 5;
     	Block CurrentBlock = null;
     	
     	if( Vegetation.debugging ) logOutput( "Spreading moss.." );
@@ -19,22 +19,24 @@ public final class Moss
 		{
 			if( MaxCycle%2 == 0 )
 			{
-				CurrentBlock = Blocks.getRandomBlock( B.getLocation() ,Material.COBBLESTONE, Material.STATIONARY_WATER );
+				CurrentBlock = Blocks.getRandomBlock( B.getLocation() ,Material.COBBLESTONE, Material.STATIONARY_WATER, 25 );
 				if( CurrentBlock != null )
 				{
 					CurrentBlock.setType(Material.MOSSY_COBBLESTONE);
+					if( Vegetation.debugging ) logOutput( "Planting at: " + B.getX() + " " + B.getY() + " " + B.getZ() );
 					MaxSpreadAmount--;
 				}
 			}
 			else
 			{
-				CurrentBlock = Blocks.getRandomBlock( B.getLocation() , Material.COBBLESTONE, Material.AIR, 5 );
+				CurrentBlock = Blocks.getRandomBlock( B.getLocation() , Material.COBBLESTONE, Material.AIR, 25 );
 				if( CurrentBlock != null )
 				{
 					if( Blocks.isAdjacentBlockofType2( CurrentBlock  , Material.STATIONARY_WATER )
 							|| Blocks.isAdjacentBlockofType2( CurrentBlock  , Material.WATER ) )
 					{
 						CurrentBlock.setType(Material.MOSSY_COBBLESTONE);
+						if( Vegetation.debugging ) logOutput( "Planting at: " + B.getX() + " " + B.getY() + " " + B.getZ() );
 						MaxSpreadAmount--;
 					}
 				}
@@ -47,6 +49,7 @@ public final class Moss
     public static boolean growSingleMoss(Block B)
     {
     	B.setType(Material.MOSSY_COBBLESTONE);
+    	if( Vegetation.debugging ) logOutput( "Planting at: " + B.getX() + " " + B.getY() + " " + B.getZ() );
     	return true;
     }
 }
