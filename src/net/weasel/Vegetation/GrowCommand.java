403e5be076db6439c2a81125869e134d26c8e8fe
zeff89 <zeff89@gmx.de>
2011-04-27 23:51:28 +0200

    fixed wrong return value for grow command

--- src/net/weasel/Vegetation/GrowCommand.java
+++ src/net/weasel/Vegetation/GrowCommand.java
@@ -172,7 +172,7 @@ public class GrowCommand implements CommandExecutor{
 						else
 						{
 							Vegetation.ActivePlayerCommands--;
-							return true;
+							return false;
 						}
 					}
 				}
