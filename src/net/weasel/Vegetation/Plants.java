ef2a774fa4059047d892a7de3768b4ff6d1d05f7
zeff89 <zeff89@gmx.de>
2011-05-07 00:49:57 +0200

    more cleaning

--- src/net/weasel/Vegetation/Plants.java
+++ src/net/weasel/Vegetation/Plants.java
@@ -6,35 +6,35 @@ import org.bukkit.Material;
 
 public class Plants 
 {
-	public void logOutput( String text ) { Vegetation.logOutput( text ); }
+	public void logOutput(String text) { Vegetation.logOutput(text); }
 	
-	private BlockCrawler blocks;
+	private BlockCrawler blockCrawler;
 	
 	public Plants(BlockCrawler b)
 	{
-		blocks = b;
+		blockCrawler = b;
 	}
 	
-	public void growPlant(Block B, Material PlantType)
+	public void growPlant(Block block, Material plantType)
 	{
 		Material[] M = { Material.YELLOW_FLOWER, Material.RED_ROSE, Material.BROWN_MUSHROOM, Material.RED_MUSHROOM, Material.PUMPKIN };
-		if( blocks.getFieldDensity( B, 3, M ) > 0.1 ) return;
+		if( blockCrawler.getFieldDensity(block, 3, M) > 0.1 ) return;
 		
-		if( Vegetation.debugging ) logOutput( "Spreading plants.." );
+		if( Vegetation.debugging ) logOutput("Spreading plants..");
 
 		int MaxSpreadAmount = 2;
 		
-		if( Vegetation.debugging ) logOutput( "Spreading Type of Plant: " + PlantType.toString() );
+		if( Vegetation.debugging ) logOutput("Spreading Type of Plant: " + plantType.toString());
 		
 		Block PlantBlock = null;
 		//Get surrounding block and place new plant
 		for( int I = 0; I < 150; I++ )
 		{
-			PlantBlock = blocks.getRandomTopBlock( B.getLocation() , Material.GRASS, Material.AIR, 3);
+			PlantBlock = blockCrawler.getRandomTopBlock(block.getLocation() , Material.GRASS, Material.AIR, 3);
 			if( PlantBlock != null )
 			{
-				PlantBlock.getRelative(BlockFace.UP).setType(PlantType);
-				if( Vegetation.debugging ) logOutput( "Planting at: " + PlantBlock.getX() + " " + PlantBlock.getY() + " " + PlantBlock.getZ() );
+				PlantBlock.getRelative(BlockFace.UP).setType(plantType);
+				if( Vegetation.debugging ) logOutput("Planting at: " + PlantBlock.getX() + " " + PlantBlock.getY() + " " + PlantBlock.getZ());
 				MaxSpreadAmount--;
 			}
 
@@ -42,10 +42,10 @@ public class Plants
 		}
 	}
 	
-	public boolean growSinglePlant(Block B, Material PlantType)
+	public boolean growSinglePlant(Block block, Material plantType)
 	{
-		B.getRelative(BlockFace.UP).setType(PlantType);
-		if( Vegetation.debugging ) logOutput( "Planting at: " + B.getX() + " " + B.getY() + " " + B.getZ() );
+		block.getRelative(BlockFace.UP).setType(plantType);
+		if( Vegetation.debugging ) logOutput("Planting at: " + block.getX() + " " + block.getY() + " " + block.getZ());
 		return true;
 	}
 }
