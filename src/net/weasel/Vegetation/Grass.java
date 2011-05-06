6d17850d12d18889f05eb465541cd0030480edb5
zeff89 <zeff89@gmx.de>
2011-05-07 00:33:36 +0200

    more commenting and cleaning

--- src/net/weasel/Vegetation/Grass.java
+++ src/net/weasel/Vegetation/Grass.java
@@ -7,27 +7,38 @@ import org.bukkit.block.BlockFace;
 
 public class Grass 
 {
-	public void logOutput( String text ) { Vegetation.logOutput( text ); }
+	public void logOutput(String text) { Vegetation.logOutput(text); }
 	
-	private BlockCrawler blocks;
+	private BlockCrawler blockCrawler;
 	private int maxGrassHeight;
 	
-	public Grass(BlockCrawler b, int height)
+	/*
+	 * generates a new Grass object
+	 * 
+	 * @param b
+	 * @param maxHeight
+	 */
+	public Grass(BlockCrawler b, int maxHeight)
 	{
-		blocks = b;
-		maxGrassHeight = height;
+		blockCrawler = b;
+		maxGrassHeight = maxHeight;
 	}
 	
-	public void growGrass( Block B )
+	/*
+	 * grows grass on give block
+	 * 
+	 * @param block
+	 */
+	public void growGrass(Block block)
 	{	
 		if( Vegetation.debugging ) logOutput( "Growing grass.." );
 
 
-		int V = B.getData();
+		int V = block.getData();
 		if( V > maxGrassHeight + 1 )
 		{
-			B.setData( (byte)(maxGrassHeight + 1) );
-			if( Vegetation.debugging ) logOutput( "Adjusting block " + B.getX() + "," + B.getY() + "," + B.getZ() + "V:" + V );
+			block.setData((byte)(maxGrassHeight + 1));
+			if( Vegetation.debugging ) logOutput("Adjusting block " + block.getX() + "," + block.getY() + "," + block.getZ() + "V:" + V);
 		}
 		else
 		{
@@ -36,8 +47,8 @@ public class Grass
 			{
 				if( V < 2 ) V = 2;
 				if( V > 10 ) V = 10;
-				B.setData( (byte)V );
-				if( Vegetation.debugging ) logOutput( "Adjusting block " + B.getX() + "," + B.getY() + "," + B.getZ() + "V:" + V );
+				block.setData((byte)V);
+				if( Vegetation.debugging ) logOutput("Adjusting block " + block.getX() + "," + block.getY() + "," + block.getZ() + "V:" + V);
 			}
 		}
 
@@ -50,33 +61,38 @@ public class Grass
 		}*/
 	}
     
-    public void mowGrass( Location BaseBlock )
+	/*
+	 * cuts down grass in an area around give location
+	 * 
+	 * @param baseBlock
+	 */
+    public void mowGrass(Location baseBlock)
     {    	
-    	int R = Math.round( 50 / 2 );
-    	double pX = BaseBlock.getX();
-    	double pZ = BaseBlock.getZ();
+    	int r = Math.round( 50 / 2 );
+    	double pX = baseBlock.getX();
+    	double pZ = baseBlock.getZ();
     	    	
-    	Block CurrentBlock = null;
+    	Block currentBlock = null;
     	
-    	for( double X = pX-R; X <= pX+R; X++ )
+    	for( double x = pX-r; x <= pX+r; x++ )
     	{
-    		for( double Z = pZ-R; Z <= pZ+R; Z++ )
+    		for( double Z = pZ-r; Z <= pZ+r; Z++ )
     		{
-    			CurrentBlock = blocks.getTopBlock(BaseBlock, X, Z, Material.AIR );
+    			currentBlock = blockCrawler.getTopBlock(baseBlock, x, Z, Material.AIR);
     			
-    			if( CurrentBlock != null )
+    			if( currentBlock != null )
     			{
-	    			if( CurrentBlock.getType() == Material.GRASS ) 
+	    			if( currentBlock.getType() == Material.GRASS ) 
 	    			{
-	    				CurrentBlock.setData( (byte)0 );
+	    				currentBlock.setData((byte)0);
 	    			}
-	    			else if( CurrentBlock.getType() == Material.RED_ROSE
-	    					|| CurrentBlock.getType() == Material.YELLOW_FLOWER 
-	    					|| CurrentBlock.getType() == Material.RED_MUSHROOM
-	    					|| CurrentBlock.getType() == Material.BROWN_MUSHROOM )
+	    			else if( currentBlock.getType() == Material.RED_ROSE
+	    					|| currentBlock.getType() == Material.YELLOW_FLOWER 
+	    					|| currentBlock.getType() == Material.RED_MUSHROOM
+	    					|| currentBlock.getType() == Material.BROWN_MUSHROOM )
 	    			{
-	    				if( CurrentBlock.getRelative(BlockFace.DOWN).getType() == Material.GRASS )
-	    					CurrentBlock.getRelative(BlockFace.DOWN).setData( (byte)0 );
+	    				if( currentBlock.getRelative(BlockFace.DOWN).getType() == Material.GRASS )
+	    					currentBlock.getRelative(BlockFace.DOWN).setData((byte)0);
 	    			}
     			}
     		}
