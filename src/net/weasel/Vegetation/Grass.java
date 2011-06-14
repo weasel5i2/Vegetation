e787d14457006114cb290ecfdf4fb64b978204f3
zeff89 <zeff89@gmx.de>
2011-06-14 23:47:58 +0200

    cleaned dependency

--- src/net/weasel/Vegetation/Grass.java
+++ src/net/weasel/Vegetation/Grass.java
@@ -2,7 +2,6 @@ package net.weasel.Vegetation;
 
 import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.block.Biome;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 
